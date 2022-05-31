fbb0ac78cf2ee5ea362dbb1de60333b9c46ddf5d
Ken <git@larrym.33mail.com>
2022-05-31 11:22:05 -0400

    com

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,10 @@
 import { defineNuxtConfig } from "nuxt";
 
 export default defineNuxtConfig({
+  modules: ["@nuxtjs/sanity"],
+  sanity: {
+    projectId: "qt2ynqua",
+		apiVersion: '2022-05-31'
+  },
   css: ["@/assets/css/styles.css"]
 });
